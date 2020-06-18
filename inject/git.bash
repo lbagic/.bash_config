a219af46bb439a06d412d09b03758f11e8002651
lbzg <lbzg.dev@gmail.com>
2020-06-18 11:01:00 +0200

    git fix

--- inject/git.bash
+++ inject/git.bash
@@ -3,8 +3,6 @@ alias gadd="git add ."
 alias gcommit="git commit -m"
 alias gstatus="git status"
 alias gpush="git push"
-alias gpull="gpull"
-alias ginitcommit="git commit -m \"Initial commit\""
-alias ginitpush="git push --set-upstream origin master"
+alias gpull="git pull"
 
-echo '(git) g [add, (init)commit, status, (init)push, pull]'
+echo '(git) g [add, commit, status, push, pull]'

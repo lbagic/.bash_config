bf9c2244e29bfc7bfe5fb28bd7813fedd778841e
lbzg <lbzg.dev@gmail.com>
2020-06-22 13:21:15 +0200

    docker updates

--- inject/2_docker.bash
+++ inject/2_docker.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
-alias dimages="docker image ls"
 alias dps="docker ps"
 alias dpsa="docker ps -a"
-alias dstart="docker start"
-alias dstop="docker stop"
-function dstartall { docker start $(docker ps -a -q); }; export -f dstartall
-function dstopall { docker stop $(docker ps -a -q); }; export -f dstopall
-alias drm="docker rm"
-alias drmall="docker system prune"
-alias drmimage="docker image rm"
-alias drmimages="docker image prune"
 
-alias dbuild="docker build -t"
-alias dcreate="docker create"
+alias dimages="docker image ls"
+alias dnetworks="docker network ls"
+alias dvolumes="docker volume ls"
+alias dsystem="docker ps -a; docker image ls; docker network ls; docker volume ls"
+
+alias dstart="docker start"
+function dstartall { docker start $(docker ps -a -q); }; export -f dstartall
+alias dstop="docker stop"
+function dstopall { docker stop $(docker ps -a -q); }; export -f dstopall
+
+alias drm="docker rm"
 alias dcompose="docker-compose -f docker-compose.yml -f docker-compose.development.yml up"
 
-echo 'docker: d [images, ps, psa, start(all), stop(all), rm(all/image/images), build, create, compose]'
+echo 'docker: d [ps, psa, images, networks, volumes, system, start(all), stop(all), rm, compose]'

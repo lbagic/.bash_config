85ccec4a4ffa4394279462d0b46979b127abebec
lbzg <lbzg.dev@gmail.com>
2020-06-22 18:06:32 +0200

    docker updates

--- inject/2_docker.bash
+++ inject/2_docker.bash
@@ -1,18 +1,22 @@
 #!/bin/bash
 alias dps="docker ps"
 alias dpsa="docker ps -a"
-
 alias dimages="docker image ls"
 alias dnetworks="docker network ls"
 alias dvolumes="docker volume ls"
 alias dsystem="docker ps -a; docker image ls; docker network ls; docker volume ls"
 
 alias dstart="docker start"
-function dstartall { docker start $(docker ps -a -q); }; export -f dstartall
 alias dstop="docker stop"
+
+function dstartall { docker start $(docker ps -a -q); }; export -f dstartall
 function dstopall { docker stop $(docker ps -a -q); }; export -f dstopall
 
 alias drm="docker rm"
-alias dcompose="docker-compose -f docker-compose.yml -f docker-compose.development.yml up"
+alias dprune="docker system prune -a"
 
-echo 'docker: d [ps, psa, images, networks, volumes, system, start(all), stop(all), rm, compose]'
+alias dcompose="docker-compose -f docker-compose.yml -f docker-compose.development.yml up"
+alias dcomp="docker-compose up"
+alias dcompdown="docker-compose down"
+
+echo 'docker: d [ps, psa, images, networks, volumes, system, start(all), stop(all), rm, prune, compose, comp, compdown]'

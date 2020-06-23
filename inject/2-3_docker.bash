c9deebd791ed1b6f3b82d64a73bf237f32827052
lbzg <lbzg.dev@gmail.com>
2020-06-23 23:38:10 +0200

    docker updates

--- inject/2-3_docker.bash
+++ inject/2-3_docker.bash
@@ -18,7 +18,8 @@ alias dprune="docker system prune -a"
 alias dup="docker-compose up"
 alias ddown="docker-compose down"
 alias ddev="docker-compose -f docker-compose.yml -f docker-compose.development.yml up"
+alias dlocal="docker-compose -f docker-compose.yml -f docker-compose.local.yml up"
 alias dstage="docker-compose -f docker-compose.yml -f docker-compose.staging.yml up"
 alias dprod="docker-compose -f docker-compose.yml -f docker-compose.production.yml up"
 
-echo 'docker: d [ps, psa, images, networks, volumes, system, start(all), stop(all), rm, prune, compose, comp, compdown, dev, stage, prod]'
+echo 'docker: d [ps, psa, images, networks, volumes, system, start(all), stop(all), rm, prune, up, down, dev, local, stage, prod]'

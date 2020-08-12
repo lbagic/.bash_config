728b3bf67f05c069dd8c5116148cbae035275bee
lbzg <lbzg.dev@gmail.com>
2020-08-12 03:13:07 +0200

    Update git

--- inject/2-1_git.bash
+++ inject/2-1_git.bash
@@ -2,5 +2,8 @@
 alias ga="git add ."
 alias gc="git commit -m"
 alias gs="git status"
+alias gpush="git push"
+alias gpusho="git push --set-upstream origin"
+alias gpull="git pull"
 
-echo 'git: ga, gc, gs'
+echo 'git: ga, gc, gs, gpull, gpush, gpusho'

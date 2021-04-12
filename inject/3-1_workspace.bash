586acb765bb230425319dafff0c66af506dfc596
Luka Bagic <luka.bagic@sintezis.co>
2021-04-12 15:47:30 +0200

    updates

--- inject/3-1_workspace.bash
+++ inject/3-1_workspace.bash
@@ -2,11 +2,6 @@
 alias ws="cd ${ws}"
 alias cfg="cd ${cfg}"
 
-skel="${ws}/skel"
-alias skel="cd ${skel}"
-pkg="${ws}/packages"
-alias pkg="cd ${pkg}"
-
 p="${ws}/projects"
 alias p="cd ${p}"
 gop="${ws}/projects/go/src/github.com/lbzg"
@@ -14,4 +9,4 @@ alias gop="cd ${gop}"
 s="${ws}/sintezis"
 alias s="cd ${s}"
 
-echo "workspace: ws, cfg, pkg, p, s, skel"
+echo "workspace: ws, cfg, p, s"
